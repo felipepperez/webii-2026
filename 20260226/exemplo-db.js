9a353778f233e3440a56dabc82eb83af472d7773
felipepperez <fpproducoes@gmail.com>
2026-02-26 21:05:16 -0400

    Aula 26022026

--- 20260226/exemplo-db.js
+++ 20260226/exemplo-db.js
@@ -3,6 +3,7 @@ const { JsonDB, Config } = require('node-json-db');
 async function main() {
     const db = new JsonDB(new Config("dados", true, true, "/"));
 
+    console.log("===Create===");
     await db.push("/usuario", { name: "João", idade: 25 });
     console.log("Salvo: /usuario");
 
@@ -10,6 +11,7 @@ async function main() {
     await db.push("/usuarios/lista[]", { id: 2, nome: "Pedro" });
     console.log("Salvo: /usuarios/lista (2 itens)");
 
+    console.log("===Read===");
     const usuario = await db.getData("/usuario");
     console.log("Lido /usuario:", usuario);
 
@@ -18,6 +20,13 @@ async function main() {
 
     const dadosCompletos = await db.getData("/");
     console.log("Raiz do banco", JSON.stringify(dadosCompletos, null, 2));
+
+    console.log("===Delete===");
+    if (lista.length > 0)
+        await db.delete(`/usuarios/lista[${lista.length - 1}]`);
+
+    console.log("===Update===");
+    await db.push(`/usuarios/lista[4]`, { id: 0, nome: "João" });
 }
 
 main().catch((err) => console.error(err));
